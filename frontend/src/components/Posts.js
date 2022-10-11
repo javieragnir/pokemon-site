74cb0309e2effcae9b8297989a09c9ab3a0fec46
javier <jjr.agnir@gmail.com>
2022-10-11 10:57:09 +0800

    make post creation modal

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import posts from '../data/posts'
 import {
   Container,
@@ -6,26 +7,69 @@ import {
   Stack,
   Paper,
   Button,
-  Typography
+  Typography,
+  Modal,
+  TextField,
+  Fade
 } from '@mui/material'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const Posts = () => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   return (
     <Container>
       <Stack direction="row" justifyContent="space-between" alignItems="flex-end">
         <Typography variant="h2">Posts</Typography>
             <Box marginBottom={1}>
-              <Button variant="contained" sx={{ height: 'max-content' }}>
+              <Button onClick={handleOpen} variant="contained" sx={{ height: 'max-content' }}>
                 Add post
               </Button>
             </Box>
       </Stack>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography variant="h5" sx={{ marginBottom: 1 }}>
+                Add post
+            </Typography>
+            <Stack spacing={2}>
+              <TextField
+
+              />
+              <TextField
+                multiline
+                rows={5}
+              />
+              <Button variant="contained">
+                Submit
+              </Button>
+            </Stack>
+          </Box>
+        </Fade>
+      </Modal>
       <Grid container spacing={2}>
         {
           posts.map(post => 
             <Grid item xs={12} key={post.id}>
-              <Paper sx={{ padding: 1 }}>         
+              <Paper sx={{ padding: 2, textAlign: 'justify', paddingRight: 4 }}>         
                 <Stack spacing={1} direction='row'>
                   <Box>
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${post.pokemon}.png`} alt={post.pokemon} />
