4b2b1702b4321b90efa904f27ff4cd9f1df663c4
javier <jjr.agnir@gmail.com>
2022-10-17 10:38:11 +0800

    display trades in descending chronological order

--- backend/controllers/trade_requests.js
+++ backend/controllers/trade_requests.js
@@ -3,7 +3,7 @@ const { TradeRequest, User } = require('../models')
 const { sequelize } = require('../util/db')
 const { tokenExtractor } = require('../util/middleware')
 
-router.get('/', async (req, res) => {
+const getTrades = async () => {
   const trades = await TradeRequest.findAll({
     include: [
       {
@@ -13,22 +13,20 @@ router.get('/', async (req, res) => {
       'offered',
       'requested',
     ],
+    order: [['createdAt', 'DESC']]
   })
+
+  return trades
+}
+
+router.get('/', async (req, res) => {
+  const trades = await getTrades()
   res.json(trades)
 })
 
 router.post('/', tokenExtractor, async (req, res) => {
-  const trade = await TradeRequest.create({...req.body, userId: req.decodedToken.id })
-  const trades = await TradeRequest.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ['id', 'username']
-      },
-      'offered',
-      'requested',
-    ],
-  })
+  await TradeRequest.create({...req.body, userId: req.decodedToken.id })
+  const trades = getTrades()
   res.json(trades)
 })
 
