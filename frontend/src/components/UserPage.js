c8e147ff8e0dd66d3a34319f5a51191ac0533cff
javier <jjr.agnir@gmail.com>
2022-10-17 11:37:42 +0800

    make grid for user page

--- frontend/src/components/UserPage.js
+++ frontend/src/components/UserPage.js
@@ -1,9 +1,22 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Typography, Grid, Box, Paper } from '@mui/material'
 
 const UserPage = () => {
   return (
     <Container>
-      <Typography>User</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Paper>
+            <Typography>Lololololol</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Box>
+            <Typography>
+              lololololololololol
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
