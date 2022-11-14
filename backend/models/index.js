376e3a9fead7055b44418720cae110d955060bab
javier <jjr.agnir@gmail.com>
2022-11-14 10:59:34 +0800

    fix deletion type stuff

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,18 +5,18 @@ const TradeComment = require('./trade_comment');
 const TradeLike = require('./trade_like');
 const TradeCommentLike = require('./trade_comment_like');
 
-User.hasMany(TradeRequest);
+User.hasMany(TradeRequest, { onDelete: 'cascade', foreignKey: { allowNull: false }, hooks: true });
 TradeRequest.belongsTo(User);
 
 TradeRequest.belongsTo(Pokemon, { as: 'offered' });
 TradeRequest.belongsTo(Pokemon, { as: 'requested' });
 
-User.hasMany(TradeComment);
+User.hasMany(TradeComment, { onDelete: 'cascade', foreignKey: { allowNull: false }, hooks: true });
 TradeComment.belongsTo(User);
-TradeRequest.hasMany(TradeComment);
+TradeRequest.hasMany(TradeComment, { onDelete: 'cascade', foreignKey: { allowNull: false }, hooks: true });
 TradeComment.belongsTo(TradeRequest);
 
-User.belongsToMany(TradeRequest, { through: TradeLike, as: 'liked_posts' });
+User.belongsToMany(TradeRequest, { through: TradeLike, as: 'liked_posts', onDelete: 'cascade' });
 TradeRequest.belongsToMany(User, { through: TradeLike, as: 'users_liked' });
 
 User.belongsToMany(TradeComment, { through: TradeCommentLike, as: 'liked_comments' });
--- frontend/src/components/ErrorAlert.jsx
+++ frontend/src/components/ErrorAlert.jsx
@@ -3,7 +3,7 @@ import { Alert, AlertTitle } from '@mui/material';
 function ErrorAlert({ errorMessage }) {
   if (errorMessage) {
     return (
-      <Alert severity="error" sx={{ width: '100%', marginBottom: 2 }}>
+      <Alert severity="error" sx={{ width: '100%', marginBottom: 2, marginTop: 2 }}>
         <AlertTitle>Error</AlertTitle>
         { errorMessage }
       </Alert>
